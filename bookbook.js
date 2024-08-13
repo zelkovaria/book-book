d1ebd414283a56d13c1a3dc4f86b7c650e5e807f
zelkovaria <hyein396@gmail.com>
2024-08-13 17:40:41 +0900

    feat: 책에 hover시 책 소개 글 표시

--- bookbook.js
+++ bookbook.js
@@ -1,11 +1,7 @@
 const $bookCon = document.querySelector(".bookCon");
 
 const createBookInfo = (bookInfo) => {
-  // let description = bookInfo.description
-  //   ? bookInfo.description.length > 30
-  //     ? bookInfo.description.subString(0, 30) + '.."'
-  //     : bookInfo.description
-  //   : "내용이 존재하지 않습니다";
+  let description = bookInfo?.description || "책 소개가 존재하지 않습니다";
 
   const newBookInfo = `
       <li class="book-item">
@@ -19,21 +15,42 @@ const createBookInfo = (bookInfo) => {
             <span class="author">${bookInfo.author}</span>
             <span class="publisher">${bookInfo.publisher}</span>
           </div>
+          
           <button class="favorite">
             <img src="/heart.png" alt="Favorite" />
           </button>
+        </div>
+        <div class="book-card-back">
+          <span class="description">${description}</span>
         </div>
       </li>
     `;
   return newBookInfo;
 };
 
-fetch("http://localhost:3000/api/search?query=aladdin")
+fetch("http://localhost:3000/api/search?query=연금술사")
   .then((response) => response.json())
   .then((data) => {
     // console.log("API 응답 데이터:", data.item);
     const bookHTML = data.item.map((book) => createBookInfo(book)).join("");
     $bookCon.innerHTML = bookHTML;
+
+    const $bookItems = document.querySelectorAll(".book-item");
+    $bookItems.forEach(($bookItem) => {
+      $bookItem.addEventListener("mouseover", () => {
+        const $bookCardBack = $bookItem.querySelector(".book-card-back");
+        const $bookCard = $bookItem.querySelector(".book-card");
+        $bookCardBack.style.display = "block";
+        $bookCard.style.display = "none";
+      });
+
+      $bookItem.addEventListener("mouseout", () => {
+        const $bookCardBack = $bookItem.querySelector(".book-card-back");
+        const $bookCard = $bookItem.querySelector(".book-card");
+        $bookCardBack.style.display = "none";
+        $bookCard.style.display = "block";
+      });
+    });
   })
   .catch((error) => {
     console.error("API 요청 중 오류 발생:", error);
