97dc2356923f35f6f3642b92a732c14b8ebe73d3
zelkovaria <hyein396@gmail.com>
2024-08-18 23:45:00 +0900

    feat: query 값에 따른 검색 결과

--- bookbook.js
+++ bookbook.js
@@ -1,4 +1,11 @@
 const $bookCon = document.querySelector(".bookCon");
+const $searchInput = document.querySelector(".searchInput");
+
+let pageSize = 8;
+let page = 1;
+let totalResults = 0;
+let groupSize = 5;
+let currentPage = 1;
 
 const createBookInfo = (bookInfo) => {
   let description = bookInfo?.description || "책 소개가 존재하지 않습니다";
@@ -28,31 +35,87 @@ const createBookInfo = (bookInfo) => {
   return newBookInfo;
 };
 
-fetch("http://localhost:3000/api/search?query=연금술사")
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log("API 응답 데이터:", data.item);
-    const bookHTML = data.item.map((book) => createBookInfo(book)).join("");
-    $bookCon.innerHTML = bookHTML;
+const pagination = (totalItem) => {
+  let html = "";
+  if (totalItem <= 8) return;
 
-    const $bookItems = document.querySelectorAll(".book-item");
-    $bookItems.forEach(($bookItem) => {
-      const $bookImage = $bookItem.querySelector(".book-image");
-      const $bookImageBack = $bookItem.querySelector(".book-image-back");
-      const $bookDetails = $bookItem.querySelector(".book-details");
+  let totalPage = Math.ceil(totalItem / pageSize);
+  let pageGroup = Math.ceil(currentPage / groupSize);
 
-      $bookImage.addEventListener("mouseover", () => {
-        $bookImageBack.style.display = "block";
-        $bookDetails.style.display = "none";
-      });
+  let lastNumber = pageGroup * groupSize;
+  if (lastNumber > totalPage) lastNumber = totalPage;
+  let firstNumber = lastNumber - (groupSize - 1);
+  if (firstNumber < 1) firstNumber = 1;
 
-      $bookImage.addEventListener("mouseout", () => {
-        $bookImageBack.style.display = "none";
-        $bookDetails.style.display = "flex";
-      });
+  const next = lastNumber + 1;
+  const prev = firstNumber - 1;
+
+  if (prev > 0) {
+    html += `<button class="pageNumber" data-page="${prev}">Prev</button>`;
+  }
+
+  for (let i = firstNumber; i <= lastNumber; i++) {
+    html += `<button class="pageNumber ${
+      i === currentPage ? "active" : ""
+    }" data-page="${i}">${i}</button>`;
+  }
+
+  if (next <= totalPage) {
+    html += `<button class="pageNumber" data-page="${next}">Next</button>`;
+  }
+
+  document.querySelector(".pageCon").innerHTML = html;
+
+  const pageButtons = document.querySelectorAll(".pageNumber");
+  pageButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      currentPage = Number(event.target.getAttribute("data-page"));
+      fetchBooks(currentPage);
     });
-  })
-  .catch((error) => {
-    console.error("API 요청 중 오류 발생:", error);
-    document.getElementById("result").innerText = "오류 발생: " + error.message;
   });
+};
+
+const fetchBooks = (page, query) => {
+  fetch(`http://localhost:3000/api/search?query=${query}&page=${page}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let totalItem = data.item.length;
+
+      const bookHTML = data.item.map((book) => createBookInfo(book)).join("");
+      $bookCon.innerHTML = bookHTML;
+
+      const $bookItems = document.querySelectorAll(".book-item");
+      $bookItems.forEach(($bookItem) => {
+        const $bookImage = $bookItem.querySelector(".book-image");
+        const $bookImageBack = $bookItem.querySelector(".book-image-back");
+        const $bookDetails = $bookItem.querySelector(".book-details");
+
+        $bookImage.addEventListener("mouseover", () => {
+          $bookImageBack.style.display = "block";
+          $bookDetails.style.display = "none";
+        });
+
+        $bookImage.addEventListener("mouseout", () => {
+          $bookImageBack.style.display = "none";
+          $bookDetails.style.display = "flex";
+        });
+      });
+
+      pagination(totalItem);
+    })
+    .catch((error) => {
+      console.error("API 요청 중 오류 발생:", error);
+      document.getElementById("result").innerText =
+        "오류 발생: " + error.message;
+    });
+};
+
+$searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    query = $searchInput.value;
+    currentPage = 1;
+    fetchBooks(currentPage, query);
+  }
+});
+
+fetchBooks(1);
